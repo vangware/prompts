44ab26cd0a045926cde28118a535dcf8c494d580
Luke Shiru <hi@luke.sh>
2023-04-06 18:20:47 -0700

    ⬆️ dependency upgrade. 🏷️ update types to be more flexible. 📄 update `README.md` with new usage examples in browser environments.

---
+++ src/QuestionObject.ts
@@ -0,0 +1,16 @@
+/**
+ * Question object.
+ *
+ * @remarks
+ * Small abstraction layer on top the `question` method of Node's `readline`
+ * module.
+ */
+export type QuestionObject = {
+	/**
+	 * Question function.
+	 *
+	 * @param query Question to ask.
+	 * @returns Promise with the answer.
+	 */
+	readonly question: (query: string) => Promise<string>;
+};
